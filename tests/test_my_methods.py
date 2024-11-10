35bc84ff4a022e906607bb030ab1ed4fc92b3834
Georgios Charitos <25990997+geo-xar@users.noreply.github.com>
2024-11-10 00:05:26 +0000

    first commit

---
+++ tests/test_my_methods.py
@@ -0,0 +1,11 @@
+import unittest
+from my_methods import get_id
+
+
+class TestGetId(unittest.TestCase):
+    def test_get_id(self):
+        self.assertEqual(get_id(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
