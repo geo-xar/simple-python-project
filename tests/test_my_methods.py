2af5346bfc043800c75190cac741b7cd991b018d
Georgios Charitos <25990997+geo-xar@users.noreply.github.com>
2024-11-10 00:53:49 +0000

    Add the project root directory to the PYTHONPATH

--- tests/test_my_methods.py
+++ tests/test_my_methods.py
@@ -1,5 +1,10 @@
 import unittest
 from my_methods import get_id
+import sys
+import os
+
+# Add the project root directory to the PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 class TestGetId(unittest.TestCase):
